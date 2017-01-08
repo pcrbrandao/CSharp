a9a8346c02a16273ffdff25da558c10b3d984929
Pedro <pcrbrandao@gmail.com>
2017-01-07 22:10:23 -0200

    um app console

---
+++ UmAppParaConsole/UmAppParaConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace UmAppParaConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MessageBox.Show("Olá, novamente\nAgora no\nMessageBox!");
+        }
+    }
+}
