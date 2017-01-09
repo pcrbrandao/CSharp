0503d298804df2d50484f668bc23118af1396985
Pedro <pcrbrandao@gmail.com>
2017-01-09 19:37:15 -0200

    Criando testes

--- UmAppParaConsole/UmAppParaConsole/Program.cs
+++ UmAppParaConsole/UmAppParaConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows.Forms;
+using System;
 
 namespace UmAppParaConsole
 {
@@ -6,7 +7,26 @@ namespace UmAppParaConsole
     {
         static void Main(string[] args)
         {
-            MessageBox.Show("Olá, novamente\nAgora no\nMessageBox!");
+            WhileCounter counter = new WhileCounter();
+            counter.Execute();
+            MessageBox.Show("Olá, você!");
+        }
+    }
+
+    public class WhileCounter
+    {
+        string CountString(int fim)
+        {
+            int count = 0;
+            string saida = "";
+            while (++count <= fim)
+                saida = saida + count.ToString() + " ";
+            return saida;
+        }
+
+        public void Execute()
+        {
+            Console.WriteLine(CountString(fim: 5));
         }
     }
 }
---
+++ UmAppParaConsole/UnitTestUmAppParaConsole/UnitTestWhileCounter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UmAppParaConsole;
+
+namespace UnitTestUmAppParaConsole
+{
+    [TestClass]
+    public class UnitTestWhileCounter
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            WhileCounter counter = new WhileCounter();
+        }
+    }
+}
