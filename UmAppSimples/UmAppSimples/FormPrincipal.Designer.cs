c9416922317ef361746139bd5acd8c0b7a5caa73
Pedro <pcrbrandao@gmail.com>
2017-01-09 18:04:42 -0200

    Tudo certo!

---
+++ UmAppSimples/UmAppSimples/FormPrincipal.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace UmAppSimples
+{
+    partial class UmAppSimplesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.welcomeLabel = new System.Windows.Forms.Label();
+            this.cabraPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.cabraPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // welcomeLabel
+            // 
+            this.welcomeLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.welcomeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.welcomeLabel.Location = new System.Drawing.Point(12, 9);
+            this.welcomeLabel.Name = "welcomeLabel";
+            this.welcomeLabel.Size = new System.Drawing.Size(510, 40);
+            this.welcomeLabel.TabIndex = 0;
+            this.welcomeLabel.Text = "Bem-vindo ao C#!";
+            this.welcomeLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // cabraPictureBox
+            // 
+            this.cabraPictureBox.BackgroundImage = global::UmAppSimples.Properties.Resources.alpame;
+            this.cabraPictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.cabraPictureBox.Location = new System.Drawing.Point(12, 52);
+            this.cabraPictureBox.Name = "cabraPictureBox";
+            this.cabraPictureBox.Size = new System.Drawing.Size(510, 249);
+            this.cabraPictureBox.TabIndex = 1;
+            this.cabraPictureBox.TabStop = false;
+            // 
+            // UmAppSimplesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Yellow;
+            this.ClientSize = new System.Drawing.Size(534, 313);
+            this.Controls.Add(this.cabraPictureBox);
+            this.Controls.Add(this.welcomeLabel);
+            this.Name = "UmAppSimplesForm";
+            this.Text = "Um App Simples";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.cabraPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label welcomeLabel;
+        private System.Windows.Forms.PictureBox cabraPictureBox;
+    }
+}
+
---
+++ UmAppSimples/UmAppSimples/FormPrincipal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace UmAppSimples
+{
+    public partial class UmAppSimplesForm : Form
+    {
+        public UmAppSimplesForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
